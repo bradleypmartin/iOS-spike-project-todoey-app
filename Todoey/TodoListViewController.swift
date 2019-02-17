c97a79c39ca613dbd7babfaf0ae4c61b055f3b12
bradleypmartin <brad.martin@ibotta.com>
2019-02-17 15:38:47 -0700

    first hardcoded cells added to table view

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["item 1", "item 2", "item 3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
+    //MARK - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 
 }
 
